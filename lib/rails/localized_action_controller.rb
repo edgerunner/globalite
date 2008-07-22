8aaee2aff1006efd01a454abfec8019807176791
Mert Torun <mert@merttorun.com>
2008-07-22 23:58:14 +0300

    Disabled ActionController resource path name localization for Rails version < 2.1.0

--- lib/rails/localized_action_controller.rb
+++ lib/rails/localized_action_controller.rb
@@ -5,14 +5,14 @@ module ActionController
     @@resources_path_names.update ({ 
       :new => :resources_path_new.l('new') , 
       :edit => :resources_path_edit.l('edit')
-    })
+    }) unless RAILS_GEM_VERSION < "2.1.0"
     
     # Reloads the localization
     def self.relocalize
       @@resources_path_names.update ({ 
         :new => :resources_path_new.l('new') , 
         :edit => :resources_path_edit.l('edit')
-      })
+      }) unless RAILS_GEM_VERSION < "2.1.0"
     end
   end
 end
